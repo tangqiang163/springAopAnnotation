32962cccc9b39ed5217c345cab1f07630aeb3842
tangqiang <18271806135@163.com>
2019-05-13 09:37:12 +0800

    随便写写 -

--- src/main/java/com/example/springaop/demo/controller/TestController.java
+++ src/main/java/com/example/springaop/demo/controller/TestController.java
@@ -5,7 +5,11 @@ import com.example.springaop.demo.test.Demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +30,21 @@ public class TestController {
     @TestAopAnnotion
     public String testAop(String test){
 
+        // 通过requestContextHolder对象的获取请求对象的属性值
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
 
+        String sessionId = requestAttributes.getSessionId();
+
+
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        System.out.println(sessionId);
+
+/*
         List list = new ArrayList<>();
 
         list.get(10);
+*/
 
 
         String str =  demo.play("uzi","lol");
